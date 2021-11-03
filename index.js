e5ba1ef06d02ee7632b2dededc79ee56133b4003
+branden <+branden_ott@outlook.com+>
2021-11-03 14:00:28 -0400

    Functions lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (blocks) {
+const hq =42
+
+    if (blocks >= hq) {
+            return blocks - hq
+    }else  {
+        return hq - blocks
+    }
+    
+};
+
+const dist = 264
+function distanceFromHqInFeet(blocks) {
+ return distanceFromHqInBlocks(blocks) * dist 
+
+};
+
+function  distanceTravelledInFeet(start, dest) {
+    if (start >= dest) { 
+        return (start - dest) * 264
+    } else { 
+            return (dest - start) * 264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let d = distanceTravelledInFeet(start, destination)
+    if (d <= 400) {
+        return 0
+    }else if (d <=2000) {
+        return (d - 400)  * .02
+    }else if (d < 2500) {
+        return 25
+    }else return 'cannot travel that far' 
+};
